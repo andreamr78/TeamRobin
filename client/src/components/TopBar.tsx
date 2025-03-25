98b97a323431cf551b280077bc46e996a7c70c4e
Merarrigs <merarri.007@outlook.com>
2025-03-24 21:48:54 -0500

    updsated Individualpage and travel card and topbar

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
-interface TopBarProps {
-    searchQuery: string;
-    setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
-}
-
-const TopBar: React.FC<TopBarProps> = ({ searchQuery, setSearchQuery }) => {
+const TopBar = () => {
     return (
-        <div>
-            <input
-                type="text"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                placeholder="Search..."
-            />
-        </div>
+      <div>
+        {Auth.loggedIn() ? (
+          <>
+            <Link to="/">Locations</Link>
+            <Link to="/saved-places">Saved Places</Link>
+            <button className="btn btn-lg btn-light m-2" onClick={Auth.logout}>
+              Logout
+            </button>
+          </>
+        ) : null}
+      </div>
     );
-};
-
+  }
 export default TopBar;
